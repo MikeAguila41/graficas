8cbb5c4dd26e86406269f67b9140885c111f0abf
Miguel aguila <A01706165@tec.mx>
2024-04-25 22:19:03 -0600

    add req

--- hola.py
+++ hola.py
@@ -3,11 +3,12 @@ import streamlit as st
 import pandas as pd
 
 #creamos el titulo de la app
-st.title('Mi primera app')
-st.header('Demo app')
-st.write('Demo de sitio con graficas')
+st.title('Titanic app')
+st.header('Titanic Graphs - App ')
+st.write('Gráficas del dataset titanic')
 
 #se imprime el archivo en la página web
 titanic_link = 'titanic.csv'
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
+
